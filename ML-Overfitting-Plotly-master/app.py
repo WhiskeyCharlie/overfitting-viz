7d28efcfdaa5244c9426c01ea184ade22a8566ee
Jonathan Simon <jnthnsimon@gmail.com>
2021-02-24 01:39:50 +0200

    Fix some UI bugs, add resplit functionality

--- ML-Overfitting-Plotly-master/app.py
+++ ML-Overfitting-Plotly-master/app.py
@@ -5,7 +5,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
 import plotly.graph_objs as go
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 from sklearn.datasets import load_boston
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
@@ -52,7 +52,7 @@ app.layout = html.Div(children=[
             style={
                 'min-width': '15%',
                 'max-height': 'calc(100vh - 85px)',
-                'overflow-y': 'auto',
+                'overflow-y': 'hidden',
                 'overflow-x': 'hidden',
             },
             children=[
@@ -64,7 +64,7 @@ app.layout = html.Div(children=[
                         options=[
                             {'label': 'Dataset #1', 'value': 'dataset #1'},
                             {'label': 'Dataset #2', 'value': 'dataset #2'},
-                            {'label': 'Custom Data', 'value': 'custom'},
+                            # {'label': 'Custom Data', 'value': 'custom'},
                             {'label': 'Dataset Degree 0', 'value': 'degree_0'},
                             {'label': 'Dataset Degree 1', 'value': 'degree_1'},
                             {'label': 'Dataset Degree 2', 'value': 'degree_2'},
@@ -82,32 +82,19 @@ app.layout = html.Div(children=[
                         clearable=False,
                         searchable=False,
                         style={
-                            'color': 'rgb(0, 0, 0)'
+                            'color': 'rgb(0, 0, 0)',
                         }
+
                     ),
-                    drc.named_dropdown(
-                        name='Click Mode (Select Custom Data to enable)',
-                        id='dropdown-custom-selection',
-                        options=[
-                            {'label': 'Add Training Data Points', 'value': 'training'},
-                            {'label': 'Add Test Data Points', 'value': 'test'},
-                            {'label': 'Remove Data point', 'value': 'remove'},
-                            {'label': 'Do Nothing', 'value': 'nothing'},
-                        ],
-                        value='training',
-                        clearable=False,
-                        searchable=False,
-                        style={
-                            'color': 'rgb(0, 0, 0)'
-                        }
-                    ),
+                    html.Br(),
                     drc.input_area(
-                        name="Select Dataset Noise Factor",
+                        name="Select Noise Factor",
                         min=0,
                         value=0,
                         id='slider-dataset-noise',
                         type='number'
                     ),
+                    html.Br(),
                     drc.input_area(
                         name="Select Dataset Sample Size",
                         min=10,
@@ -115,6 +102,7 @@ app.layout = html.Div(children=[
                         id="slider-sample-size",
                         type='number'
                     ),
+                    html.Br(),
                     drc.input_area(
                         name='Select Model Polynomial Degree',
                         min=0,
@@ -122,7 +110,11 @@ app.layout = html.Div(children=[
                         value=1,
                         id="slider-polynomial-degree",
                         type='number'
-                    )
+                    ),
+                    html.Br(),
+                    html.Br(),
+                    html.Button('Resample Train/Test', id='resample-btn'),
+                    html.Br(),
                 ]),
             ]
         ),
@@ -230,18 +222,9 @@ def format_yhat(model):
     return coefficient_string
 
 
-@app.callback(Output('dropdown-custom-selection', 'disabled'),
-              [Input('dropdown-dataset', 'value')])
-def disable_custom_selection(dataset):
-    return dataset != 'custom'
-
-
 @app.callback(Output('custom-data-storage', 'children'),
-              [Input('graph-regression-display', 'clickData')],
-              [State('dropdown-custom-selection', 'value'),
-               State('custom-data-storage', 'children'),
-               State('dropdown-dataset', 'value')])
-def update_custom_storage(clickData, selection, data, dataset):
+              [Input('graph-regression-display', 'clickData')])
+def update_custom_storage(data):
     if data is None:
         data = {
             'train_X': [1, 2],
@@ -251,24 +234,6 @@ def update_custom_storage(clickData, selection, data, dataset):
         }
     else:
         data = json.loads(data)
-        if clickData and dataset == 'custom':
-            selected_X = clickData['points'][0]['x']
-            selected_y = clickData['points'][0]['y']
-
-            if selection == 'training':
-                data['train_X'].append(selected_X)
-                data['train_y'].append(selected_y)
-            elif selection == 'test':
-                data['test_X'].append(selected_X)
-                data['test_y'].append(selected_y)
-            elif selection == 'remove':
-                while selected_X in data['train_X'] and selected_y in data['train_y']:
-                    data['train_X'].remove(selected_X)
-                    data['train_y'].remove(selected_y)
-                while selected_X in data['test_X'] and selected_y in data['test_y']:
-                    data['test_X'].remove(selected_X)
-                    data['test_y'].remove(selected_y)
-
     return json.dumps(data)
 
 
@@ -277,31 +242,21 @@ def update_custom_storage(clickData, selection, data, dataset):
                Input('slider-sample-size', 'value'),
                Input('slider-polynomial-degree', 'value'),
                Input('slider-dataset-noise', 'value'),
-               Input('custom-data-storage', 'children')])
-def update_graph(dataset, sample_size, degree, noise_factor, custom_data=None):
+               Input('resample-btn', 'n_clicks')])
+def update_graph(dataset, sample_size, degree, noise_factor, n_clicks=0,
+                 split_random_state=RANDOM_STATE):
+    ctx = dash.callback_context
+    if ctx.triggered:
+        button_is_event = ctx.triggered[0]['prop_id'].split('.')[0] == 'resample-btn'
+        if button_is_event:
+            np.random.seed(n_clicks or RANDOM_STATE)
+            split_random_state = np.random.randint(100)
     # Generate base data
-    if dataset == 'custom':
-        custom_data = json.loads(custom_data)
-        X_train = np.array(custom_data['train_X']).reshape(-1, 1)
-        y_train = np.array(custom_data['train_y'])
-        X_test = np.array(custom_data['test_X']).reshape(-1, 1)
-        y_test = np.array(custom_data['test_y'])
-        X_range = np.linspace(-5, 5, 300).reshape(-1, 1)
+    X, y = make_dataset(dataset, RANDOM_STATE, sample_size, noise_factor)
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=int(X.shape[0] * 0.15), random_state=split_random_state)
 
-        trace_contour = go.Contour(
-            x=np.linspace(-5, 5, 300),
-            y=np.linspace(-5, 5, 300),
-            z=np.ones(shape=(300, 300)),
-            showscale=False,
-            hoverinfo='none',
-            contours=dict(coloring='lines'),
-        )
-    else:
-        X, y = make_dataset(dataset, RANDOM_STATE, sample_size, noise_factor)
-        X_train, X_test, y_train, y_test = \
-            train_test_split(X, y, test_size=int(X.shape[0] * 0.15), random_state=RANDOM_STATE)
-
-        X_range = np.linspace(X.min() - 0.5, X.max() + 0.5, sample_size).reshape(-1, 1)
+    X_range = np.linspace(X.min() - 0.5, X.max() + 0.5, sample_size).reshape(-1, 1)
 
     # Create Polynomial Features
     poly = PolynomialFeatures(degree=degree, include_bias=False)
@@ -344,8 +299,6 @@ def update_graph(dataset, sample_size, degree, noise_factor, custom_data=None):
         line=dict(width=4)
     )
     data = [trace0, trace1, trace2]
-    if dataset == 'custom':
-        data.insert(0, trace_contour)
 
     layout = go.Layout(
         title=f"MSE: {train_error:.3f} (Train Data) "
@@ -365,12 +318,13 @@ def update_graph(dataset, sample_size, degree, noise_factor, custom_data=None):
 @app.callback(Output('graph-fitting-display', 'figure'),
               [Input('dropdown-dataset', 'value'),
                Input('slider-sample-size', 'value'),
-               Input('slider-dataset-noise', 'value')])
-def update_fitting_graph(dataset, sample_size, noise_factor):
+               Input('slider-dataset-noise', 'value'),
+               Input('resample-btn', 'n_clicks')])
+def update_fitting_graph(dataset, sample_size, noise_factor, n_clicks=0):
     max_degree_to_check = 10
     X, y = make_dataset(dataset, RANDOM_STATE, sample_size, noise_factor)
     X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size=int(X.shape[0] * 0.15), random_state=RANDOM_STATE)
+        train_test_split(X, y, test_size=int(X.shape[0] * 0.15), random_state=n_clicks or RANDOM_STATE)
 
     train_errors = []
     test_errors = []
